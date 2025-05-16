e60d1dcc8a2e984f5f42f34365e447fff2cddebc
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2025-05-16 14:10:42 -0700

    Add host/ip info + DDNS reporting

--- network-status.1m.sh
+++ network-status.1m.sh
@@ -9,6 +9,10 @@
 #  <xbar.dependencies>bash,openssl</xbar.dependencies>
 #  <xbar.abouturl>https://develotters.com</xbar.abouturl>
 
+#  <xbar.var>string(VAR_DOMAIN=""): domain name</xbar.var>
+#  <xbar.var>string(VAR_SUBDOMAIN=""): subdomain name</xbar.var>
+#  <xbar.var>string(VAR_API_KEY=""): api key</xbar.var>
+
 OPENSSL_EXEC='/opt/homebrew/bin/openssl'
 SITE='linkedin.com:443'
 
@@ -23,6 +27,20 @@ else
     esac
 fi
 
+HOSTNAME=$(hostname)
+LOCAL_IP=$(ipconfig getifaddr en0)
+PUBLIC_IP=$(curl --silent 'https://api.ipify.org')
+DDNS_RS=''
+
+if [ -n "$VAR_API_KEY" ]; then
+    DDNS_RS=$(curl --silent "https://www.dynadot.com/set_ddns?containRoot=false&domain=${VAR_DOMAIN}&subDomain=${VAR_SUBDOMAIN}&pwd=${VAR_API_KEY}&ttl=300&type=A&ip=${PUBLIC_IP}")
+fi
+
+echo '---'
+echo "H: $HOSTNAME"
+echo "L: $LOCAL_IP"
+echo "P: $PUBLIC_IP"
+echo "$DDNS_RS"
 echo '---'
 echo "$SITE"
 echo "$RS"
