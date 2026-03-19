a84aa518b96883d6709e908f4500ce214b8b850d
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2026-03-19 14:19:17 -0700

    Remove DDNS reporting feature

--- network-status.1m.sh
+++ network-status.1m.sh
@@ -9,13 +9,8 @@
 #  <xbar.dependencies>bash,openssl</xbar.dependencies>
 #  <xbar.abouturl>https://develotters.com</xbar.abouturl>
 
-#  <xbar.var>string(VAR_DOMAIN=""): domain name</xbar.var>
-#  <xbar.var>string(VAR_SUBDOMAIN=""): subdomain name</xbar.var>
-#  <xbar.var>string(VAR_API_KEY=""): api key</xbar.var>
-
 OPENSSL_EXEC='/opt/homebrew/bin/openssl'
 SITE='linkedin.com:443'
-DDNS_ENABLED='false'
 
 RS=$("$OPENSSL_EXEC" s_client -connect "$SITE" <<< 'GET /' 2>&1)
 if [ "$?" -ne 0 ]; then
@@ -31,17 +26,11 @@ fi
 HOSTNAME=$(hostname)
 LOCAL_IP=$(ipconfig getifaddr en0)
 PUBLIC_IP=$(curl --silent --fail 'https://api.ipify.org') || PUBLIC_IP='N/A'
-DDNS_RS=''
-
-if [ -n "$VAR_API_KEY" ] && [ "$DDNS_ENABLED" == 'true' ]; then
-    DDNS_RS=$(curl --silent "https://www.dynadot.com/set_ddns?containRoot=false&domain=${VAR_DOMAIN}&subDomain=${VAR_SUBDOMAIN}&pwd=${VAR_API_KEY}&ttl=300&type=A&ip=${PUBLIC_IP}")
-fi
 
 echo '---'
 echo "H: $HOSTNAME"
 echo "L: $LOCAL_IP"
 echo "P: $PUBLIC_IP"
-if [ -n "$DDNS_RS" ]; then echo "$DDNS_RS"; fi
 echo '---'
 echo "$SITE"
 echo "$RS"
